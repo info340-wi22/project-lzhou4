90a426eaf61c49f77710b7c1557cdfb05509c91e
Jenny Sun <jennysun@uw.edu>
2022-02-28 18:22:25 -0800

    add component files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+//import { NavBar } from './Navigation';
+import RestaurantRanks from "./RestaurantList";
 
-function App() {
+import RESTAURANT_LISTINGS from '..data/restaurant_listings.json';
+export default function App(props) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container-fluid">
+      <div className="row">
+        <div className="col-2">
+          <RestaurantRanks restaurants={RESTAURANT_LISTINGS}/>
+        </div>
+      </div>
     </div>
   );
 }
 
-export default App;

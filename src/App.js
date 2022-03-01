6f229be2b8e91aaee959a2e634f3d6680a0b8784
lzhou4 <lzhou4@uw.edu>
2022-02-28 18:04:36 -0800

    angel's commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState }from 'react';
+import FormCompontent from './form';
+import { RestraurantList } from './RestrList';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
 
-export default App;
+  const addForm = (restraurantName, restraurantDescript, restraurantDirection) => {
+    const newRestr = {
+      inputRestrauant: restraurantName,
+      inputDescript: restraurantDescript,
+      inputDirection: restraurantDirection,
+    }
+    return newRestr;
+    // setInput(newRestr)
+  }
+
+// define function addForm pass as a prop to FormCompontent
+
+    return (
+      <div className="App">
+        <FormCompontent addForm={addForm}/>
+        <RestraurantList />
+      </div>
+    );
+  }
+
+  export default App;
